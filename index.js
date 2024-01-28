5b349d98e42e95d626ac9ce20cec412ff0fd2ca8
juanjbot <juan.jose.botero1@gmail.com>
2024-01-28 07:17:43 -0500

    ya sabe

---
+++ index.js
@@ -0,0 +1,17 @@
+let nro1 =[5,8,10,12,18,21];
+
+let nro2 =[4,2,18,9,2,5];
+
+
+function sumarara() {
+  for (let i=0; i<nro1.length; i++)
+    {
+      let suma = nro1[i]+ nro2[i];
+      let resta= nro1[i]- nro2[i];
+      
+      console.log(nro1[i]+ " + " + nro2[i] + " = " + resta+""+ suma);
+    }
+                   } 
+sumarara();
+
+
